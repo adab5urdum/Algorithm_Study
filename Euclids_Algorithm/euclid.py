7f48385f2b1d21d465781a74dfb820643faf608d
J. A. Jackson <81472652+adab5urdum@users.noreply.github.com>
2023-03-09 16:10:57 +0000

    setting up the structure

---
+++ Euclids_Algorithm/euclid.py
@@ -0,0 +1,16 @@
+# Euclid's Algorithm in python
+
+def euclid(number1, number2):
+    
+    return 0
+
+def main():
+    num1 = 0
+    num2 = 0
+
+    answer = euclid(num1, num2)
+
+    print(answer)
+
+
+main()
